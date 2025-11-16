8717078c83b1defd4cf90addc3920f69095b7ad1
RZDK <kleshnya.dimon@gmail.com>
2025-11-16 14:46:57 +1000

    Add files via upload

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.post('/api/messages', async (req, res) => {
     res.status(201).json(newMessage);
 });
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
